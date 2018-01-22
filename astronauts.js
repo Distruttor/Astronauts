4c800e6ad1d98cb209c03c01997b3e6e54cf978a
Distruttor <mirko.schicchi@studenti.unitn.it>
2018-01-22 16:11:19 +0100

    Add astronauts.js file

---
+++ astronauts.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const astronauts = express.Router();
+
+const astronautsList = [];
+
+
+module.exports = astronauts;
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+var astronauts = require('./astronauts');
+
 const app = express();
 
 var port = process.env.PORT || 9000;
+
+app.use('/astronauts', astronauts);
 
 app.listen(port, () => {
     console.log("Server is listening on port " + port);
