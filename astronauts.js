85e92b7504d9e42553e5dc427ee68921020143ca
Distruttor <mirko.schicchi@studenti.unitn.it>
2018-01-22 16:37:15 +0100

    Add possibility to insert a new astrounat

--- astronauts.js
+++ astronauts.js
@@ -7,5 +7,12 @@ const astronauts = express.Router();
 
 const astronautsList = [];
 
+astronauts.post('/', (req, res) => {
+    let newAstrounat = req.body.newAstrounat;
+
+    newAstrounat.ID = uuid();
+    astronautsList.push(newAstrounat);
+    res.json(newAstrounat);
+});
 
 module.exports = astronauts;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var astronauts = require('./astronauts');
 
 const app = express();
+app.use(bodyParser.json());
 
 var port = process.env.PORT || 9000;
 
